428af568284e40da540249596c2df48097895af9
mkarasu2005 <mkarasu2662005@gmail.com>
2026-02-25 20:28:16 +0530

    feat(user): implement User schema and add test user creation in server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
+import User from "./models/user.model"
 
 dotenv.config();
 
@@ -15,3 +16,14 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const testUser = async () => {
+  const user = await User.create({
+    email: "test@example.com",
+    password: "123456"
+  });
+
+  console.log("User created:", user);
+};
+
+testUser();
